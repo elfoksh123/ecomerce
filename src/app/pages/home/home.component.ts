451bbebdedc0185d903e5ad57a087e28feab18f6
unknown <mohamedf0ouaad123@gmail.com>
2025-02-14 16:57:05 +0200

    dist

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,19 +3,62 @@ import { ProductsService } from '../../core/services/products/products.service';
 import { error } from 'console';
 import { Hiproduct } from '../../shared/interface/hiproduct';
 import { CategoriesService } from '../../core/services/category/categories.service';
+import { Icategory } from '../../shared/interface/icategory';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  // import { ProductsService } from './../../core/services/products/products.service';
-// templimport { CategoriesService } from './../../core/services/category/categories.service';
+  imports: [CarouselModule],
+ 
 templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
   private readonly productsService=inject(ProductsService);
-  private readonly categoriesService=inject(CategoriesService)
+  private readonly categoriesService=inject(CategoriesService);
+  custommain: OwlOptions = {
+   loop: true,
+   mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    autoplay:true,
+    
+    dots: false,
+     navSpeed: 700,
+    navText: ['', ''],
+   items:1,
+  nav: false
+   }
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: false
+  }
   products:Hiproduct[]=[];
+  Categories:Icategory[]=[];
   getProductData():void{
     this.productsService.getAllProducts().subscribe({
       next:(res)=>{
@@ -32,7 +75,8 @@ export class HomeComponent implements OnInit {
   getCategoryData():void{
     this.categoriesService.getALLCategories().subscribe({
       next:(res)=>{
-        console.log(res);
+        console.log(res.data);
+        this.Categories = res.data;
         
 
       },
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,24 +1,62 @@
-import { Component } from '@angular/core';
-import{FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms"
+import { Component, inject } from '@angular/core';
+import{AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms"
+import { AuthService } from '../../core/services/auth/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
-  templateUrl: './register.component.html',
+  imports: [ReactiveFormsModule,],
+  // import { AuthService } from './../../core/services/auth/auth.service';
+
+templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  private readonly authService=inject(AuthService)
+  isloading:boolean=false;
  
   register:FormGroup=new FormGroup({
-    name:new FormControl(null),
-    email:new FormControl(null),
-    password:new FormControl(null),
-    rePassword:new FormControl(null),
-    phone:new FormControl(null),
-  });
+    name:new FormControl(null ,[Validators.required ,Validators.minLength(3),Validators.maxLength(20)]),
+    email:new FormControl(null,[Validators.required ,Validators.email]),
+    password:new FormControl(null,[Validators.required ,Validators.pattern(/^[A-Z]\W{7,}$/)]),
+    rePassword:new FormControl(null ,[Validators.required ]),
+    phone:new FormControl(null,[Validators.required , Validators.pattern(/^01[0125][0-9]{8}$/)]),
+  },{validators :this.confirmPassword});
   submitForm():void{
-    console.log(this.register);
+   if(this.register.valid){
+    this.isloading=true
+    this.authService.sendRegisterForm(this.register.value).subscribe({
+      next:(res)=>{
+        console.log(res);
+        if(res.message === 'success'){
+          
+        }
+        this.isloading=false
+        
+
+      },
+      error:(err:HttpErrorResponse)=>{
+        console.log(err);
+        err.error.message
+        this.isloading=false;
+        
+      }
+    })
     
+   }
+
+  }
+  confirmPassword(group: AbstractControl){
+   const password= group.get('password')?.value
+   const rePassword= group.get('rePassword')?.value
+   return password === rePassword ? null : {mismatch:true}
+  //  if(password === rePassword){
+  //   return null;
+
+
+  //  }else{
+  //   return {mismatch:true}
+  //  }
 
   }
 
